7ebe3dc4cc7f11dd9db9b85d869178c7f57863f2
stefanstefanovic94 <stefan.stef94@gmail.com>
2020-07-19 12:45:34 +0200

    Updates

---
+++ precache-manifest.74fdf77cb2d9208fa782fa518f083298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b459471a6b17298ffef0c064104e7f",
+    "url": "/People/index.html"
+  },
+  {
+    "revision": "171df29a76f99f99fd90",
+    "url": "/People/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "40d4e043d01d8f47a49b",
+    "url": "/People/static/css/main.75ff8e34.chunk.css"
+  },
+  {
+    "revision": "171df29a76f99f99fd90",
+    "url": "/People/static/js/2.af85797c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/People/static/js/2.af85797c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d4e043d01d8f47a49b",
+    "url": "/People/static/js/main.de443a25.chunk.js"
+  },
+  {
+    "revision": "7928cac7bb74040ea8a2",
+    "url": "/People/static/js/runtime-main.9ca265bc.js"
+  }
+]);
